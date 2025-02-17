d5c9fa8a83a06105fdf7ccdb2036e42bdb467df2
iltomsky <nathan.arnouts2012@gmail.com>
2025-02-17 18:47:26 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    Maqueen_V5.motorRun(Maqueen_V5.Motors.All, Maqueen_V5.Dir.CW, 100)
+    if (Maqueen_V5.Ultrasonic() >= 20) {
+        Maqueen_V5.motorStop(Maqueen_V5.Motors.All)
+    }
+    basic.pause(1000)
 })
